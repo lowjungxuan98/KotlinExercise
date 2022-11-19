a2a0b01d22d5fe2e8b35d996b244072b2faa6521
lowjungxuan <lowjungxuan@gmail.com>
2022-11-19 21:00:34 +0800

    Notification

--- app/src/main/java/com/lowjungxuan/kotlinexercise/notification/NotificationFragment.kt
+++ app/src/main/java/com/lowjungxuan/kotlinexercise/notification/NotificationFragment.kt
@@ -23,6 +23,7 @@ import androidx.fragment.app.Fragment
 import com.lowjungxuan.kotlinexercise.MainActivity
 import com.lowjungxuan.kotlinexercise.R
 import com.lowjungxuan.kotlinexercise.databinding.FragmentNotificationBinding
+import com.lowjungxuan.mylibrary.BlinkEffect
 
 class NotificationFragment : Fragment() {
     private var _binding: FragmentNotificationBinding? = null
@@ -45,6 +46,7 @@ class NotificationFragment : Fragment() {
         _binding = FragmentNotificationBinding.inflate(inflater, container, false)
         mainActivity = (activity as MainActivity)
         binding.btnNotification.setOnClickListener {
+            BlinkEffect.blink(binding.btnNotification)
             if (ContextCompat.checkSelfPermission(
                     mainActivity.applicationContext,
                     Manifest.permission.POST_NOTIFICATIONS,
